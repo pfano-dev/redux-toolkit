dbbffef7a3ccacd2d85764c6a936ea4d1985cf39
muleya pfano <79614241+pfano-dev@users.noreply.github.com>
2023-06-22 23:33:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const store = require("./app/store");
+
+const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+
+console.log("Intial state", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("updated: ", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
+
+unsubscribe();
